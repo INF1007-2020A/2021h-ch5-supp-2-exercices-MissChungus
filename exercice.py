64ed4661431e69afc0b2e9decb2e3e66bd479bb3
MissChungus <telesanti58@hotmail.com>
2021-02-20 18:01:14 -0500

    complété

--- exercice.py
+++ exercice.py
@@ -4,21 +4,56 @@
 import math
 
 def get_num_letters(text):
-	return 0
+	for char in text:
+		if char.isalnum() == False:
+			text = text.replace(char, '')
+	return len(text)
 
 def get_word_length_histogram(text):
-	return [0]
+	for char in text:
+		if char.isalnum() == False and char != ' ':
+			text = text.replace(char, '')
+	words = text.split()
+	max = 0
+	for word in words:
+		if len(word) > max:
+			max = len(word)
+	histogram = [0 for length in range(max + 1)]
+	for word in words:
+		histogram[len(word)] += 1
+	return histogram
 
 def format_histogram(histogram):
 	ROW_CHAR = "*"
+	format = ''
+	for i in range(1, len(histogram)):
+		if i > 9:
+			format += str(i) + ' ' + ROW_CHAR*histogram[i] + '\n'
+		else:
+			format += ' ' + str(i) + ' ' + ROW_CHAR * histogram[i] + '\n'
 
-	return ""
+	return format
 
 def format_horizontal_histogram(histogram):
 	BLOCK_CHAR = "|"
 	LINE_CHAR = "¯"
-
-	return ""
+	maximum = max(histogram)
+	valeur = maximum
+	compteur = 0
+	forme = ''
+	del histogram[0]
+	while compteur < maximum:
+		for i in range(len(histogram)):
+			if histogram[i] + compteur == maximum:
+				forme += BLOCK_CHAR
+				histogram[i] -= 1
+			else:
+				forme += ' '
+		compteur += 1
+		valeur -= 1
+		forme += '\n'
+	forme += LINE_CHAR*(len(histogram)+1)
+	return forme
 
 
 if __name__ == "__main__":
